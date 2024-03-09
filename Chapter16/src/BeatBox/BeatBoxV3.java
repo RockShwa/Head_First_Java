f65c43771c1e1ad0346cb1019e0e1d5a34cec250
Jeff Tipps <jag2k2@gmail.com>
2024-03-09 16:26:05 -0600

    Adding Final BeatBox

--- Chapter16/src/BeatBox/BeatBoxV3.java
+++ Chapter16/src/BeatBox/BeatBoxV3.java
@@ -1,13 +1,30 @@
 package BeatBox;
 import javax.sound.midi.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.*;
 import static javax.sound.midi.ShortMessage.*;
 
-public class BeatBoxV2 {
+public class BeatBoxV3 {
+    // TODO: Pop up a dialog box when you select a pattern that asks if you want to save your current pattern 
+    // TODO: Random pattern button (JUnit to test?)
+    private JList<String> incomingList;
+    private JTextArea userMessage;
     private ArrayList<JCheckBox> checkBoxList; // store all checkboxes in an ArrayList
+
+    private Vector<String> listVector = new Vector<>(); // This uses object locks
+    private HashMap<String, boolean[]> otherSeqsMap = new HashMap<>();
+
+    private String userName;
+    private int nextNum;
+
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
     private Sequencer sequencer;
     private Sequence sequence;
     private Track track;
@@ -22,7 +39,28 @@ public class BeatBoxV2 {
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
     public static void main(String[] args) {
-        new BeatBoxV2().buildGUI();
+        if (args.length == 0) {
+            System.out.println("You didn't pass anything to the command line!");
+        } else {
+            new BeatBoxV3().startUp(args[0]); // command line argument for screen name
+        }
+    }
+
+    public void startUp(String name) {
+        userName = name;
+        // open up connection to server (Using Sockets because they work better with I/O Streams)
+        try {
+            Socket socket = new Socket("127.0.0.1", 4242);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.submit(new RemoteReader()); 
+            socket.close(); // not sure where to put this
+        } catch (Exception ex) {
+            System.out.println("Couldn't connect-you'll have to play alone.");
+        }
+        setUpMidi();
+        buildGUI();
     }
 
     public void buildGUI() {
@@ -34,7 +72,6 @@ public class BeatBoxV2 {
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
-
         JButton start = new JButton("Start");
         start.addActionListener(e -> buildTrackAndStart());
         buttonBox.add(start);
@@ -61,6 +98,24 @@ public class BeatBoxV2 {
         JButton loadFile = new JButton("Restore Pattern");
         loadFile.addActionListener(e -> readFile());
         buttonBox.add(loadFile);
+
+        JButton sentIt = new JButton("SendIt");
+        sentIt.addActionListener(e -> sendMessageAndTracks()); // send message/current beat to server
+        buttonBox.add(sentIt);
+
+        userMessage = new JTextArea();
+        userMessage.setLineWrap(true);
+        userMessage.setWrapStyleWord(true);
+        JScrollPane messageScroller = new JScrollPane(userMessage);
+        buttonBox.add(messageScroller);
+
+        // This is where the incoming messages are displayed, lets you look and select messages
+        incomingList = new JList<>();
+        incomingList.addListSelectionListener(new MyListSelectionListener());
+        incomingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane theList = new JScrollPane(incomingList);
+        buttonBox.add(theList);
+        incomingList.setListData(listVector);
 
         Box nameBox = new Box(BoxLayout.Y_AXIS);
         for (String instrumentName : instrumentNames) {
@@ -161,6 +216,47 @@ public class BeatBoxV2 {
         sequencer.setTempoFactor(tempoFactor * tempoMultiplier);
     }
 
+    private void sendMessageAndTracks() {
+        boolean[] checkBoxState = new boolean[256];
+        for (int i = 0; i < 256; i++) {
+            JCheckBox check = checkBoxList.get(i);
+            if (check.isSelected()) {
+                checkBoxState[i] = true;
+            }
+        }
+        try {
+            out.writeObject(userName + nextNum++ + ": " + userMessage.getText());
+            out.writeObject(checkBoxState);
+        } catch (IOException e) {
+            System.out.println("Terribly sorry. Could not send it to the server.");
+        }
+        userMessage.setText("");
+    }
+
+    // This tells us when the user made a selection from the list of messages. We immediately load
+    // the associated pattern (in the HashMap otherSeqMap) and start playing it
+    public class MyListSelectionListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent lse) {
+            String selected = incomingList.getSelectedValue();
+            if (selected != null) {
+                // now go to the map, and change the sequence
+                boolean[] selectedState = otherSeqsMap.get(selected);
+                changeSequence(selectedState);
+                sequencer.stop();
+                buildTrackAndStart();
+            }
+        }
+    }
+
+    // This method is called when the user selects something from the list. We immediatley change the
+    // pattern to the one the selected
+    private void changeSequence(boolean[] checkBoxState) {
+        for (int i = 0; i < 256; i++) {
+            JCheckBox check = checkBoxList.get(i);
+            check.setSelected(checkBoxState[i]);
+        }
+    }
+
     // This makes events for one instument at a tiem, for all 16 beats. So it might get an int[]
     // for the Bass drum, and each index in the array will hold either the key of that instrument 
     // or a zero. If it's a zero, the instrument is not supposed to play at that beat. Otherwise, 
@@ -234,5 +330,33 @@ public class BeatBoxV2 {
         // using the new state of the chcekboxes in the Arraylist
         sequencer.stop();
         buildTrackAndStart();
+    }
+
+    public class RemoteReader implements Runnable {
+        // This reads the data from the server (data is the name and checkbox state)
+        public void run() {
+            try {
+                Object obj;
+                while ((obj = in.readObject()) != null) {
+                    System.out.println("got an object from the server");
+                    System.out.println(obj.getClass());
+
+                    // When message comes in, we deserialize the two objects which we want to add to
+                    // the JList component
+                    String nameToShow = (String) obj;
+                    boolean[] checkBoxState = (boolean[]) in.readObject();
+                    otherSeqsMap.put(nameToShow, checkBoxState);
+
+                    // Adding to a JList is a two step thing: you keep a Vector of the lists data
+                    // (Vector is an old fashioned ArrayList) and then tell the JList to use that 
+                    // Vector as it's source for what to display in the list
+                    listVector.add(nameToShow);
+                    incomingList.setListData(listVector);
+                }
+            // Multi-catch: If you want to catch multiple exceptions but do the same thing with them
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
